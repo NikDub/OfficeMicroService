75aaab55a86214c544f45b5f1a4a764cdbb7f800
Nikita Dubovski <nik.dubovski@gmail.com>
2022-12-02 21:11:18 +0300

    change at officestore

--- OfficeMicroService/Data/Models/DTO/OfficeDTO.cs
+++ OfficeMicroService/Data/Models/DTO/OfficeDTO.cs
@@ -4,7 +4,10 @@
     {
         public string Photo_id { get; set; }
         public string Status { get; set; }
-        public string Address { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public string OfficeNumber { get; set; }
         public string RegistryPhoneNumber { get; set; }
     }
 }
